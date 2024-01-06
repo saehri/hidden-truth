d5b950b452e66ca164a138042c1de17dbd924bfd
Saepul Bahri <bahreesaepul1@gmail.com>
2024-01-06 10:01:35 +0700

    build: chapter selection tab

--- src/components/ui/ChapterSelectionTab.tsx
+++ src/components/ui/ChapterSelectionTab.tsx
@@ -1,6 +1,8 @@
 import {Dispatch, SetStateAction, useState} from 'react';
 import {motion} from 'framer-motion';
 
+import ChapterCard from './ChapterCard';
+
 type TabTypes = 'special' | 'main' | 'premium';
 
 interface TabContent extends React.HTMLAttributes<HTMLDivElement> {
@@ -82,36 +84,7 @@ function TabContent({activeTab}: TabContent) {
       animate='show'
       className='min-h-[57.77%] flex gap-1 mb-8'
     >
-      <ChapterCard />
-      <ChapterCard />
-      <ChapterCard />
-      <ChapterCard />
+      <ChapterCard chapterId='pemilu24' />
     </motion.div>
-  );
-}
-
-function ChapterCard() {
-  return (
-    <motion.a
-      href=''
-      variants={{
-        rest: {opacity: 0, x: 50},
-        show: {opacity: 1, x: 0},
-      }}
-      whileHover={{y: -10}}
-    >
-      <article
-        style={{clipPath: 'polygon(9% 0, 100% 0%, 91% 100%, 0% 100%)'}}
-        className='min-w-48 3xl:min-w-72 h-full p-4 flex items-end text-base border-[1.5px] border-border relative after:absolute after:top-0 after:left-0 after:w-full after:h-full after:bg-yellow-600 after:rounded-full after:pointer-events-none after:blur-md after:opacity-0 after:hover:opacity-20 after:transition-opacity'
-      >
-        <h4 className='uppercase italic text-lg text-[#FBE886] relative z-10'>
-          Chapter Name
-        </h4>
-        <span
-          className='absolute block w-full h-full top-0 left-0 bg-gradient-to-t from-slate-950 via-transparent to-transparent'
-          style={{clipPath: 'polygon(9% 0, 100% 0%, 91% 100%, 0% 100%)'}}
-        ></span>
-      </article>
-    </motion.a>
   );
 }
