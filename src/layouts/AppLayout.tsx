61d6dc9b73e64d49baea2bca466b200f6cf8bf26
Saepul Bahri <bahreesaepul1@gmail.com>
2024-01-10 11:05:20 +0700

    fixed: bug where the screen is overflowing on mobile

--- src/layouts/AppLayout.tsx
+++ src/layouts/AppLayout.tsx
@@ -8,12 +8,12 @@ export default function AppLayout({children}: AppLayout) {
   return (
     <main
       id='main'
-      className='bg-slate-950 h-screen w-screen grid place-items-center'
+      className='bg-slate-950 h-full w-screen flex flex-col fixed top-0 left-0'
     >
-      <section className='w-full h-full 2xl:h-max max-w-[2080px] 3xl:mx-auto'>
+      <section className='w-full h-full 2xl:h-max max-w-[2080px] 3xl:mx-auto grid place-items-center'>
         <div
           id='game__screen-size'
-          className='w-full h-full 2xl:h-max 2xl:pt-[calc((9/20)*100%)] border border-border relative bg-[#08222D]'
+          className='w-full h-full 2xl:h-max 2xl:pt-[calc((9/20)*100%)] md:border border-border relative bg-[#08222D]'
         >
           <div
             id='game__container'
