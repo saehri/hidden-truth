9165530a0eca0e14797ea604b2554663c08fadfd
Saepul Bahri <bahreesaepul1@gmail.com>
2024-01-11 19:58:43 +0700

    refactore: made it more reusable

--- src/components/ui/BackButton.tsx
+++ src/components/ui/BackButton.tsx
@@ -7,36 +7,54 @@ import {
 
 import Button from './Button';
 import Icons from './Icons';
+import {twMerge, ClassNameValue} from 'tailwind-merge';
 
 interface BackButton extends React.HTMLAttributes<HTMLButtonElement> {
-  buttonName: string;
+  buttonName?: string;
   goBackTo: ActivePageTypes;
+  iconStyle?: ClassNameValue;
+  iconType?: 'back' | 'close';
 }
 
 export default function BackButton({
   buttonName,
+  iconStyle,
   goBackTo,
-  ...rest
+  className,
+  iconType = 'back',
 }: BackButton) {
   const {setActivePage} = useContext(ActivePageContext);
 
   return (
     <Button
       onClick={() => setActivePage(goBackTo)}
-      {...rest}
-      className='ml-auto absolute top-0 left-0 flex gap-3 items-center p-0'
+      className={twMerge(
+        'absolute z-50 flex gap-3 items-center p-0',
+        className
+      )}
     >
-      <span className='bg-yellow-800/50 block border border-border border-t-0 relative z-20 border-l-0 p-1 xl:p-2 pr-3 sm:pr-4 xl:pr-8 md:pl-2 rounded-br-2xl'>
-        <Icons.Back className='w-4 h-4 lg:w-5 lg:h-5 3xl:w-8 3xl:h-8' />
+      <span
+        className={twMerge(
+          'bg-yellow-800/50 block border border-border border-t-0 relative z-20 border-l-0 p-1 xl:p-2 pr-3 sm:pr-4 xl:pr-8 md:pl-2 rounded-br-2xl',
+          iconStyle
+        )}
+      >
+        {iconType === 'back' ? (
+          <Icons.Back className='w-4 h-4 lg:w-5 lg:h-5 3xl:w-8 3xl:h-8' />
+        ) : (
+          <Icons.CloseCircled className='w-4 h-4 lg:w-5 lg:h-5 3xl:w-8 3xl:h-8' />
+        )}
       </span>
 
-      <motion.span
-        initial={{x: -20, opacity: 0}}
-        animate={{x: 0, opacity: 1}}
-        className='text-border text-xs lg:text-sm 3xl:text-base uppercase font-[500] text-yellow-500'
-      >
-        {buttonName}
-      </motion.span>
+      {buttonName?.length && (
+        <motion.span
+          initial={{x: -20, opacity: 0}}
+          animate={{x: 0, opacity: 1}}
+          className='text-border text-xs lg:text-sm 3xl:text-base uppercase font-[500] text-yellow-500'
+        >
+          {buttonName}
+        </motion.span>
+      )}
     </Button>
   );
 }
