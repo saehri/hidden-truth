29fd7625d6cc1d834bde6dffd2a5a357b1fa4e3e
Saepul Bahri <bahreesaepul1@gmail.com>
2024-01-12 12:57:08 +0700

    fixed: error page naming

--- src/components/ui/EnergyBar.tsx
+++ src/components/ui/EnergyBar.tsx
@@ -23,7 +23,7 @@ export default function EnergyBar() {
         {userEnergy.energy}/{userEnergy.max_energy}
       </span>
 
-      {activePage.location !== 'game' && (
+      {activePage.location !== 'gamePage' && (
         <Button className='absolute -right-3 top-1/2 -translate-y-1/2 bg-primary/95 border border-border-light rounded-full'>
           <Icons.AddWhite className='w-3 h-3 lg:w-4 lg:h-4 3xl:w-6 3xl:h-6' />
         </Button>
@@ -34,7 +34,7 @@ export default function EnergyBar() {
 
 function EneryCountdownDisplay() {
   return (
-    <div className='absolute leading-3 -bottom-[14px] sm:-bottom-[18px] xl:-bottom-[18px] 3xl:-bottom-[26px] uppercase  left-1/2 -translate-x-1/2 text-[8px] xl:text-[10px] 3xl:text-sm w-max bg-gradient-to-t from-primary via-yellow-500 to-yellow-50 px-1 md:p-[2px] rounded-br-md rounded-bl-md text-[#0e0c04] border border-border border-t-0'>
+    <div className='absolute leading-3 -bottom-[14px] sm:-bottom-[14px] xl:-bottom-[18px] 3xl:-bottom-[26px] uppercase  left-1/2 -translate-x-1/2 text-[8px] xl:text-[10px] 3xl:text-sm w-max bg-gradient-to-t from-primary via-yellow-500 to-yellow-50 px-1 md:p-[2px] rounded-br-md rounded-bl-md text-[#0e0c04] border border-border border-t-0'>
       +1 dalam 2:49
     </div>
   );
