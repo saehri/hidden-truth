f38e184c5fc9d616bbec8e70c82ac82eee146f6c
Saepul Bahri <bahreesaepul1@gmail.com>
2024-01-13 19:02:44 +0700

    add: new assets

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,12 @@ import {defineConfig} from 'vite';
 import react from '@vitejs/plugin-react-swc';
 import viteTsconfigPaths from 'vite-tsconfig-paths';
 import {VitePWA} from 'vite-plugin-pwa';
+import jotaiDebugLabel from 'jotai/babel/plugin-debug-label';
+import jotaiReactRefresh from 'jotai/babel/plugin-react-refresh';
 
 export default defineConfig({
   plugins: [
-    react(),
+    react({babel: {plugins: [jotaiDebugLabel, jotaiReactRefresh]}}),
     viteTsconfigPaths(),
     VitePWA({registerType: 'autoUpdate'}),
   ],
